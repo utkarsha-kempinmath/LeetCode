6a31d572a50a9ba2f820d3aa5f3e9920dcf2cc8f
Utkarsha Kempinmath <utkarshakempinmath1725@gmail.com>
2026-06-29 11:17:16 +0530

    Time: 461 ms (5.39%) | Memory: 48 MB (6.69%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for(int i=0; i<nums.length; i++){
+            for(int j=0; j<nums.length; j++){
+                if(nums[j]==0 && j<nums.length-1){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+    }
+}
