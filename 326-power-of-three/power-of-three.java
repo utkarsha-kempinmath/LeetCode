a4c00853a44ab40f88fd6d42b05e95b60c09a43b
Utkarsha Kempinmath <utkarshakempinmath1725@gmail.com>
2026-07-04 16:14:33 +0530

    Time: 7 ms (100.00%) | Memory: 46.1 MB (44.25%) - LeetSync

---
+++ 326-power-of-three/power-of-three.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<=0) return false;
+        if(n==1) return true;
+        if(1162261467%n == 0) return true;
+        return false;
+    }
+}
