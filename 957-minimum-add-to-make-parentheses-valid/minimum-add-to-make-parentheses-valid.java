40f25e75ac11a0c0177d922b52da1482cb5037a9
Utkarsha Kempinmath <utkarshakempinmath1725@gmail.com>
2026-07-04 10:55:04 +0530

    Time: 1 ms (67.79%) | Memory: 42.7 MB (87.52%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open = 0;
+        int close = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                open++;
+            }
+            else{
+                if(open>0) open--;
+                else close++;
+            }
+        }
+        return open+close;
+    }
+}
