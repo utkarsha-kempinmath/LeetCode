711ea772407f1ce07eee50574c03e0506160b4af
Utkarsha Kempinmath <utkarshakempinmath1725@gmail.com>
2026-07-05 08:05:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (35.62%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+        int a=1;
+        int b=2;
+        for(int i=3; i<=n; i++){
+            int c = b+a;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+}
