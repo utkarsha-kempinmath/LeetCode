c90acbf16e0b2b5cfff0b6245641f537a8a57fb8
Utkarsha Kempinmath <utkarshakempinmath1725@gmail.com>
2026-08-10 18:13:55 +0530

    Time: 3 ms (86.02%) | Memory: 43.3 MB (58.08%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2 != 0) return false;
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if((ch =='(' || ch == '{' || ch == '[')){
+                st.push(ch);
+            }
+            else {
+                if(st.isEmpty()) return false;
+                if(ch == ')' && st.peek() != '(') return false;
+                else if(ch == '}' && st.peek() != '{') return false;
+                else if(ch == ']' && st.peek() != '[') return false;
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}
